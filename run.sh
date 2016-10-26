2ed41277e1a39479e904490448914820246d36de
Tim Douglas <neurotech@gmail.com>
2016-10-26 14:20:46 +1100

    Added simple run script to test execution times

---
+++ run.sh
@@ -0,0 +1,6 @@
+start_time=`date +%s`
+rm config/db.json && rm lib/schema/ddl.json && rm lib/tables/registry.json && node index.js
+end_time=`date +%s`
+run_time=`expr $end_time - $start_time`
+minutes=`expr $run_time / 60`
+echo Total time to run: `echo $minutes`minutes.
