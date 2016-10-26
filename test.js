299f6185ac04e7d1caaf96fd019cd6deb4b5ffc3
Tim Douglas <neurotech@gmail.com>
2016-10-26 14:20:33 +1100

    Updated tests

--- test.js
+++ test.js
@@ -1,25 +1,27 @@
 'use strict';
 const test = require('ava');
 const config = require('./config');
-const schema = require('./lib/schema');
 const manifest = require('./lib/manifest');
+const generate = require('./lib/schema/generate');
 
 test('Valid config', t => {
   t.is(typeof config.get('api.url'), 'string', 'url OK');
   t.is(typeof config.get('api.key'), 'string', 'key OK');
   t.is(typeof config.get('api.secret'), 'string', 'secret OK');
   t.is(typeof config.get('db.host'), 'string', 'host OK');
-  t.is(typeof config.get('db.port'), 'number', 'port OK');
+  t.is(typeof config.get('db.port'), 'string', 'port OK');
   t.is(typeof config.get('db.user'), 'string', 'user OK');
   t.is(typeof config.get('db.password'), 'string', 'password OK');
-});
-
-test('GET /schema/latest', async t => {
-  const latest = await schema.fetch();
-  t.is(typeof latest, 'object');
 });
 
 test('GET /account/self/file/sync', async t => {
   const listing = await manifest.fetch();
   t.is(typeof listing, 'object');
+});
+
+test('Generate Schema', async t => {
+  generate()
+    .then(results => {
+      t.is(typeof results, 'array');
+    });
 });
