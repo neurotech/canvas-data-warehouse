06515d1de3e7568cf2464055517d0de6e9951733
Tim Douglas <neurotech@gmail.com>
2017-01-30 11:34:58 +1100

    Typo fix

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ rm config/db.json && rm lib/schema/ddl.json && rm lib/tables/registry.json && no
 end_time=`date +%s`
 run_time=`expr $end_time - $start_time`
 minutes=`expr $run_time / 60`
-echo Total time to run: `echo $minutes`minutes.
+echo Total time to run: `echo $minutes` minutes.
